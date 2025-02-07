4fb3ed74ad0a69c944e40c067d48bf9bf8446c12
woshihekai2020 <hekai2017@qq.com>
2025-02-07 11:38:23 +0800

    torch_1_tensor_full

--- 2_Torch/1_tensor.py
+++ 2_Torch/1_tensor.py
@@ -3,18 +3,46 @@ from __future__ import print_function
 import torch
 
 #1: tensor init
+print("\n 张量初始化")
 x = torch.empty(5, 3)
-print("\n torch.empty(5, 3): \n", x)
+print(" torch.empty(5, 3): \n  ", x)
 x = torch.rand(5, 3)
-print("\n torch.rand(5, 3): \n", x)
+print(" torch.rand(5, 3): \n  ", x)
 x = torch.zeros(5, 3, dtype= torch.long)
-print("\n torch.zeros(5, 3): \n", x)
+print(" torch.zeros(5, 3): \n  ", x)
 
 #2: create a tensor with SP content
+print("\n\n 初始化特定内容张量")
 x = torch.tensor([5.5, 3])
-print("\n create tensor with SP content: torch.tensor([5.5, 3]) \n", x)
+print(" create tensor with SP content: torch.tensor([5.5, 3]) \n  ", x)
 x = x.new_ones(5, 3, dtype= torch.double)
-print("\n new_ones(5, 3) : \n", x)
+print(" new_ones(5, 3) : \n  ", x)
 x = torch.randn_like(x, dtype= torch.float)
-print("\n create tensor base on a tensor: \n", x)
-print("\n 获取张量的维度信息： \n", x.size0())
+print(" create tensor base on a tensor: \n  ", x)
+print(" 获取张量的维度信息： \n  ", x.size())
+
+#3: tensor compute
+print( "\n\n 张量计算")
+y = torch.randn(5, 3)
+print( " tentsor y : \n  ", y )
+print( " tensor x add tensor y: x + y \n  ", x + y )
+print( " torch.add(x, y) : \n  ", torch.add(x, y) )
+
+#4: tensor change
+print( "\n\n 改变张量内容")
+result = torch.empty(5, 3)
+torch.add( x, y, out= result )
+print( " torch.add(x, y, out= result) , result: \n  ", result )
+y.add_( x )
+print( " change content with _y.add_(x)==> y= : \n  ", y )
+print( " use like numpy: x[:, i] \n  ", x[:, 1] )
+
+print( "\n 使用 torch.view 改变张量尺寸:")
+x = torch.randn(4, 4)
+y = x.view( 16 )
+z = x.view( -1, 8 )
+print( x.size(), y.size(), z.size() )
+
+x = torch.randn( 1 )
+print( "\n x = x.item, \n x = : \n", x )
+print( " x.item() = : \n", x.item() )
